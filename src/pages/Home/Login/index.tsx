c0e43f879680ca038c4e7959bf331c26b4541b5a
Alan <socyware@gmail.com>
2023-04-19 17:37:19 -0300

    install react-hook-form

--- src/pages/Home/Login/index.tsx
+++ src/pages/Home/Login/index.tsx
@@ -1,17 +1,30 @@
 import ButtonIcon from "components/ButtonIcon";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import "./styles.css";
 
+type FormData = {
+  username: string;
+  password: string;
+}
 
 const Login = () => {
+  const { register, handleSubmit } = useForm<FormData>();
+
+  const onSubmit =(formData : FormData) =>{
+    console.log('Sucesso')
+
+  };
+
 
   return (
     <div className="container-login">
       <div className="base-card login-card">
         <h1>LOGIN</h1>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
             <input
+              {...register("username")}
               type="text"
               className="form-control base-input"
               placeholder="Email"
@@ -20,6 +33,7 @@ const Login = () => {
           </div>
           <div className="mb-2">
             <input
+              {...register("password")}
               type="password"
               className="form-control base-input "
               placeholder="Password"
@@ -28,7 +42,7 @@ const Login = () => {
           </div>
 
           <div className="login-submit">
-            <Link to="/Movies">
+            <Link to="/movies">
               <ButtonIcon text="Fazer login" />
             </Link>
           </div>
