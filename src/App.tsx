0cec8761e8770575509937a662920aafca28ccf3
Alan <socyware@gmail.com>
2023-04-19 16:42:13 -0300

    MovieFlix-project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./assets/styles/custom.scss";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
