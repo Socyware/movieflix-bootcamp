7a02ad4ebccf4014d7957ddb7a4468e929a98ac6
Alan <socyware@gmail.com>
2023-04-22 13:05:59 -0300

    Saving auth data to localStorage

--- src/pages/Home/Login/index.tsx
+++ src/pages/Home/Login/index.tsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import { requestBackendLogin } from "util/requests";
 import { useState } from "react";
 import "./styles.css";
+import { getAuthData, saveAuthData } from "util/storage";
 
 type FormData = {
   username: string;
@@ -17,6 +18,9 @@ const Login = () => {
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
       .then((response) => {
+        saveAuthData(response.data);
+        const token = getAuthData().access_token;
+        console.log('TOKEN GERADO: ' + token);
         setHasError(false);
         console.log("SUCESSO", response);
       })
--- src/util/storage.ts
+++ src/util/storage.ts
@@ -3,9 +3,10 @@ const tokenKey = "authData";
 type LoginResponse = {
   access_token: string;
   token_type: string;
+  refresh_token: string;
   expires_in: number;
   scope: string;
-  userFirstName: string;
+  userName: string;
   userId: number;
 };
 
